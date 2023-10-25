9237b4cd3fe05ee5ca475fb3d0e0f18c5ac69e04
Rahul Sah <rahul-kprinfo>
2023-10-25 23:18:05 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 style={{display:"flex", justifyContent:"center"}}>
+       Hell Welcome to new react Project
+      </h1>
+    </>
+  )
+}
+
+export default App
